6da5ab4cf6ec94ee432faa03baaa25d16f233fa2
Percipient24 <jcpigm@rit.edu>
2017-11-02 13:32:00 -0400

    Seek and Arrive

---
+++ Assets/Scripts/Arriver.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Look at how clean and simple this guy is!
+// All the complexity is handled by Mover
+// And we can focus on just what's different about our Arriver
+public class Arriver : Mover {
+
+	public GameObject target;
+	public float threshold = 2f;
+
+	protected override void Start () {
+		base.Start ();
+	}
+
+	protected override void CalcSteering() {
+		if (target) {
+			// the next line is a fancy toggle comment ... make it read //* instead of /* and see what happens
+			/*
+			ApplyForce (Arrive (target.transform.position, threshold));
+			/*/
+			velocity += Arrive (target.transform.position, threshold, radius + 0.5f);
+			//*/
+		}
+	}
+}
---
+++ Assets/Scripts/Seeker.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Look at how clean and simple this guy is!
+// All the complexity is handled by Mover
+// And we can focus on just what's different about our Seeker
+public class Seeker : Mover {
+
+	public GameObject target;
+
+	protected override void Start () {
+		base.Start ();
+	}
+
+	protected override void CalcSteering() {
+		if (target) {
+			// the next line is a fancy toggle comment ... make it read /* instead of //* and see what happens
+			//*
+			ApplyForce( Seek (target.transform.position));
+			/*/
+			velocity += Seek (target.transform.position);
+			//*/
+		}
+	}
+}
